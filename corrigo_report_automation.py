154405aff37bb5bfb7035f5a1a08f26bcd3f338f
Hanuman Abhishek Kopparapu <abhi.kopparapu@gmail.com>
2019-09-30 12:01:14 -0500

    Update corrigo_report_automation.py

--- corrigo_report_automation.py
+++ corrigo_report_automation.py
@@ -31,7 +31,7 @@ driver.find_element_by_xpath("//td[@class='menuitem'][contains(text(),'Reports')
 driver.implicitly_wait(3000)
 driver.find_element_by_xpath("/html[1]/body[1]/div[2]/div[8]/table[1]/tbody[1]/tr[2]/td[1]").click()
 
-#Open Custodial Reports
+#Open Reports by bringing the report link into view
 elem = driver.find_element_by_xpath("/html[1]/body[1]/table[1]/tbody[1]/tr[1]/td[1]/form[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[4]/table[1]/tbody[1]/tr[1]/td[1]/span[1]/table[1]/tbody[1]/tr[1]/td[2]/div[1]/a[1]")
 coordinates = elem.location_once_scrolled_into_view # returns dict of X, Y coordinates
 self.driver.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
@@ -40,19 +40,19 @@ elem.click()
 #click on Desired Report
 driver.find_element_by_xpath("//a[contains(text(),'Custodial No-Show Report (current month)')]").click()
 
-#Input range of dates
+#Input range of dates. I am using yesterday's date to today  daily reporting
 driver.find_element_by_xpath("//input[@id='tabBuilder__ctl0___FIELD_woCreated_CONTROL___edDate_Start_t']").send_keys(date.today()-timedelta(days=1))
 driver.find_element_by_xpath("//input[@id='tabBuilder__ctl0___FIELD_woCreated_CONTROL___edDate_End_t']").send_keys(date.today())
 
-#To switch windows get window handles
+#Clicking on "Generate Report" opens new window, so to switch between the windows we need to get window handles
 window_before = driver.window_handles[0]
 driver.find_element_by_xpath("//td[contains(text(),'Generate Report')]").click()
 window_after = driver.window_handles[1]
 
-#switching from 1 window to other
+#Switching from one window to other
 driver.switch_to_window(window_after)
 
-#downloading data as csv
+#Downloading data as csv
 driver.find_element_by_xpath("//a[@id='report_ctl05_ctl04_ctl00_ButtonLink']").click()
 driver.find_element_by_xpath("//a[contains(text(),'CSV (comma delimited)')]").click()
 
@@ -72,7 +72,7 @@ Noshows_DB = pd.read_csv("Noshows_DB")
 frames = ['Noshows_DB','data']
 Noshows_DB = pd.con(frames)
 
-
+#This masterfile can be used in tableau for daily reporting
 
 
 
